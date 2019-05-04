124f4d8936da684fb7b30fcb94e258182ec17bcc
hardy1334 <hardychugh34@gmail.com>
2019-05-05 04:03:54 +0530

    tracks shown for artist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import $ from 'jquery';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   constructor () {
@@ -9,12 +10,9 @@ class App extends React.Component {
     this.state = {
       artists: {},
       artistTracks: {},
-      artistName: 'Cher',
-      search: [],
+      artistName: '',
       display: [],
-      trackCount: {
-        len: '',
-      },
+      unavailable: '',
     };
   }
 
@@ -25,19 +23,6 @@ class App extends React.Component {
     this.setState ({
       display: items,
     });
-    console.log (this.state.display);
-  }
-
-  componentDidUpdate () {
-    console.log (this.state.artistTracks);
-    let items = JSON.parse (localStorage.getItem ('item'));
-
-    if (items.length !== this.state.display.length) {
-      this.setState ({
-        display: items,
-      });
-    }
-    console.log (this.state.display);
   }
 
   onChange = e => {
@@ -58,46 +43,71 @@ class App extends React.Component {
         this.setState ({
           artists: data.artist,
         });
+      })
+      .catch (err => {
+        this.setState ({
+          unavailable: 'Please Enter Artist Name Correctly',
+        });
       });
 
     // get artist tracks from last.fm api
 
     fetch (
-      `https://ws.audioscrobbler.com/2.0/?method=user.getartisttracks&user=rj&artist=${this.state.artistName}&api_key=${apiKey}&format=json`
+      `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks
+&artist=${this.state.artistName}&api_key=${apiKey}&format=json`
     )
       .then (tracks => tracks.json ())
       .then (tracks => {
         this.setState ({
-          artistTracks: tracks.artisttracks,
+          artistTracks: tracks.toptracks.track,
+        });
+      })
+      .catch (err => {
+        this.setState ({
+          unavailable: 'Please Enter Artist Name Correctly',
         });
       });
 
-    /*let trackCount = {...this.state.trackCount};
-    trackCount.len = Object.keys (this.state.artistTracks).length;
-    this.setState ({trackCount});
-    console.log (this.state.trackCount.len);*/
-    //    localStorage.setItem ('artist', this.state.artistName);
+    if (this.state.display) {
+      this.state.display.unshift (this.state.artistName);
+    }
 
-    this.state.display.unshift (this.state.artistName);
     let uniqueArtists = [...new Set (this.state.display)];
     localStorage.setItem ('item', JSON.stringify (uniqueArtists));
+    let items = JSON.parse (localStorage.getItem ('item'));
+
+    this.setState ({
+      display: items,
+    });
   };
 
   searchHistory = e => {
     $ ('#searchbox').toggle ();
   };
 
+  biography () {
+    if (this.state.artists.bio.content === '') {
+      return `Sorry, Bio of this ${this.state.artistName} is not available`;
+    }
+    return this.state.artists.bio.content;
+  }
+
   render () {
     let artistBio = (
       <div>
-        <h2>Here are some results for {this.state.artists.name}</h2>
-        <div className="card">
+
+        <h2 className="pb-3 pt-5">
+          Here are some results for {this.state.artists.name}
+        </h2>
+        <div className="card bio">
           <div className="card-title ml-3 mt-3">
             <h3>{this.state.artists.name}'s Bio</h3>
           </div>
           <div className="card-body ">
             <small>
-              {this.state.artists.bio ? this.state.artists.bio.content : null}
+              {this.state.artists.bio
+                ? this.biography ()
+                : 'Sorry,There is nothing available'}
             </small>
           </div>
         </div>
@@ -106,7 +116,7 @@ class App extends React.Component {
 
     let statsArtist = (
       <div>
-        <div className="card mt-5">
+        <div className="card mt-5 bio">
           <div className="card-title ml-3 mt-3">
             <h3>Stats</h3>
           </div>
@@ -136,7 +146,7 @@ class App extends React.Component {
                 {this.state.artists.ontour ? this.state.artists.ontour : null}
               </div>
               <div className="col-md-6">
-                <b>Url:</b>
+                <b>Bio:</b>
                 {this.state.artists.url ? this.state.artists.url : null}
               </div>
 
@@ -149,13 +159,13 @@ class App extends React.Component {
 
     let similarArtist = (
       <div className="mt-5">
-        <h2>Similar Artists</h2>
-        <div className="row">
-          <div className="card ml-5 " style={{width: '18rem'}}>
+        <h2 className="pb-3">Similar Artists</h2>
+        <div className="row ">
+          <div className="card bio " style={{width: '18rem'}}>
             <img
               className="card-img-top"
               src="https://lastfm-img2.akamaized.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"
-              alt="Card image cap"
+              alt="Card  cap"
               height="200"
             />
             <div className="card-title ">
@@ -165,46 +175,32 @@ class App extends React.Component {
                   : null}
               </h4>
             </div>
-            <small>
-              <a className="ml-2">
-                {this.state.artists.similar
-                  ? this.state.artists.similar.artist[0].url
-                  : null}
-              </a>
+            <small className="pb-3">
+
+              {this.state.artists.similar
+                ? this.state.artists.similar.artist[0].url
+                : null}
+
             </small>
           </div>
 
-          <div className="card ml-5" style={{width: '18rem'}}>
+          <div className="card ml-5 bio" style={{width: '18rem'}}>
             <img
               className="card-img-top"
               src="https://lastfm-img2.akamaized.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"
-              alt="Card image cap"
+              alt="Card "
               height="200"
             />
-            <div className="card-title ">
-              <h4 className="ml-3 mt-3 ">
-                {this.state.artists.similar
-                  ? this.state.artists.similar.artist[1].name
-                  : null}
-              </h4>
-            </div>
-            <small>
-              <a className="ml-2">
-                {this.state.artists.similar
-                  ? this.state.artists.similar.artist[1].url
-                  : null}
-              </a>
-            </small>
           </div>
 
         </div>
         <br /><br />
         <div className="row">
-          <div className="card ml-5" style={{width: '18rem'}}>
+          <div className="card bio" style={{width: '18rem'}}>
             <img
               className="card-img-top"
               src="https://lastfm-img2.akamaized.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"
-              alt="Card image cap"
+              alt="Card star"
               height="200"
             />
             <div className="card-title ">
@@ -214,20 +210,20 @@ class App extends React.Component {
                   : null}
               </h4>
             </div>
-            <small>
-              <a className="ml-2">
-                {this.state.artists.similar
-                  ? this.state.artists.similar.artist[2].url
-                  : null}
-              </a>
+            <small className="pb-3">
+
+              {this.state.artists.similar
+                ? this.state.artists.similar.artist[2].url
+                : null}
+
             </small>
           </div>
 
-          <div className="card ml-5" style={{width: '18rem'}}>
+          <div className="card bio ml-5" style={{width: '18rem'}}>
             <img
               className="card-img-top"
               src="https://lastfm-img2.akamaized.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"
-              alt="Card image cap"
+              alt="Cap"
               height="200"
             />
             <div className="card-title ">
@@ -237,12 +233,12 @@ class App extends React.Component {
                   : null}
               </h4>
             </div>
-            <small>
-              <a className="ml-2">
-                {this.state.artists.similar
-                  ? this.state.artists.similar.artist[3].url
-                  : null}
-              </a>
+            <small className="pb-3">
+
+              {this.state.artists.similar
+                ? this.state.artists.similar.artist[3].url
+                : null}
+
             </small>
           </div>
 
@@ -250,59 +246,84 @@ class App extends React.Component {
         <br />
       </div>
     );
-
-    let tracks = (
-      <div>
-        <div className="card">
-          hghg 898
-        </div>
-      </div>
-    );
+    let searched = <p className="pl-3">You haven't searched yet</p>;
 
     return (
-      <div className="main mt-5">
+      <div>
+        <Navbar />
+        <div className="main mt-5">
+          <div className="form-group has-search">
+            <span className="fas fa-search form-control-feedback" />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search Your Favourite Artist here......"
+              onChange={this.onChange}
+              value={this.state.artistName}
+              name="artistName"
+              onClick={this.searchHistory}
+            />
+            <div className="card" id="searchbox">
 
-        <div className="form-group has-search">
-          <span className="fas fa-search form-control-feedback" />
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search Your Favourite Artist here......"
-            onChange={this.onChange}
-            value={this.state.artistName}
-            name="artistName"
-            onClick={this.searchHistory}
-          />
-          <div className="card" id="searchbox">
-            <span className="ml-5 pt-3">
-              <i className="far fa-clock" />
-              Search history
-            </span><hr />
+              <span className="ml-3 pt-3 row">
+                <i className="far fa-clock pt-1" />
+                <p className="ml-2">Search history</p>
+              </span>
 
-            {this.state.display.map (item => {
-              return (
-                <span>
-                  <p className="ml-5">{item}</p>
-                  <hr />
-                </span>
-              );
-            })}
+              <hr />
+              {this.state.display
+                ? this.state.display.map ((item, id) => {
+                    return (
+                      <span key={id}>
+                        <p className="ml-5">{item}</p>
+                        <hr />
+                      </span>
+                    );
+                  })
+                : searched}
+            </div>
 
+            <br />
+
+            <button
+              className="btn btn-primary col-md-12 col-lg-12 col-xl-12"
+              onClick={this.getData}
+            >
+              Search
+            </button>
           </div>
+          <div className="mt-5">
+            {this.state.artistName
+              ? <h2 className="mb-4">{this.state.artistName}'s Top 10 Songs</h2>
+              : null}
 
-          <br />
-
-          <button
-            className="btn btn-primary col-md-12 col-lg-12 col-xl-12"
-            onClick={this.getData}
-          >
-            Search
-          </button>
+            {this.state.artistTracks[0]
+              ? this.state.artistTracks.map ((item, id) => {
+                  if (id < 11) {
+                    return (
+                      <div className="card" key={id}>
+                        <p className="ml-3 mt-2">{item.name}</p>
+                      </div>
+                    );
+                  }
+                })
+              : null}
+          </div>
+          {this.state.artists.bio ? artistBio : null}
+          {this.state.artists.stats ? statsArtist : null}
+          <br /><br /><br />
+          {this.state.artists.similar ? <h3>Similar Artists</h3> : null}
+          {this.state.artists.similar
+            ? this.state.artists.similar.artist.map ((item, id) => {
+                return (
+                  <div className="card bio mt-3 mb-5">
+                    <h4 className="ml-5 mt-3">{item.name}</h4>
+                    <p className="ml-5">{item.url}</p>
+                  </div>
+                );
+              })
+            : null}
         </div>
-        {this.state.artistTracks.track ? tracks : null}
-        {this.state.artists.bio ? artistBio : null}
-        {this.state.artists.stats ? statsArtist : null}
-        {this.state.artists.similar ? similarArtist : null}
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,21 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, BrowserRouter} from 'react-router-dom';
 
 class Navbar extends Component {
   render () {
     return (
-      <nav className=" navbar-fixed-top navbar-default navbar navbar-expand-lg navbar-light bg-success mb-5">
-        <Link className="navbar-brand" to="/">
-          {this.props.name.name ? this.props.name.name : 'Cher'}
-        </Link>
+      <nav className="navbar navbar-light bg-light">
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/tracks">
-                Tracks <span className="sr-only">(current)</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/bio">Bio</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/similar">Similar</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/tags">Tags</Link>
-            </li>
-          </ul>
-        </div>
+        <BrowserRouter>
+
+          <Link className="navbar-brand" to="/" id="title">
+            <h3>
+              Artist Search
+            </h3>
+          </Link>
+
+        </BrowserRouter>
+
       </nav>
     );
   }
